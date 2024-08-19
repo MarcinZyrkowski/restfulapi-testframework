508d320118844c26a88611fea84f92d50072b9ce
mzyrkowski <marcinzyrkowski7@gmail.com>
2024-08-19 11:42:53 +0200

    introducing annotation

--- src/test/java/org/example/BaseTest.java
+++ src/test/java/org/example/BaseTest.java
@@ -1,11 +1,10 @@
 package org.example;
 
+import org.example.annotations.Full;
 import org.example.client.module.item.ItemClient;
 import org.example.controller.module.item.ItemController;
-import org.junit.jupiter.api.Tag;
 
-@Tag("full")
-public class BaseTest {
+public class BaseTest implements Full {
 
   protected ItemController itemController = new ItemController();
   protected ItemClient itemClient = new ItemClient();

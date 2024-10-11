610ad4f45041e1c4b06bffa44ce8ec00007ad013
mzyrkowski <marcinzyrkowski7@gmail.com>
2024-10-11 09:42:04 +0200

    replacing javafaker with datafaker

--- src/main/java/org/example/utils/FakerUtils.java
+++ src/main/java/org/example/utils/FakerUtils.java
@@ -1,12 +1,11 @@
 package org.example.utils;
 
-import com.github.javafaker.Faker;
-import com.github.javafaker.service.FakeValuesService;
-import com.github.javafaker.service.RandomService;
-import java.util.Locale;
+
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import net.datafaker.Faker;
+import net.datafaker.service.FakeValuesService;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class FakerUtils {
@@ -16,5 +15,5 @@ public class FakerUtils {
 
   @Getter
   private static final FakeValuesService fakeValuesService =
-      new FakeValuesService(Locale.getDefault(), new RandomService());
+      new FakeValuesService();
 }
